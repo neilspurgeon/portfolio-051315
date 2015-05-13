26217dee62f77758a6076facafb99fb2370716cf
Neil Spurgeon <neilspurgeon@gmail.com>
2015-05-13 14:28:43 -0700

    init

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+// Gruntfile.js
+
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+
+      // when this task is run, lint the Gruntfile and all js files in src
+      build: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/magic.min.js': 'src/js/magic.js'
+        }
+      }
+    },
+
+    // compile sass stylesheets to css -----------------------------------------
+    sass: {
+      options: {
+            sourceMap: false
+        },
+      dist: {
+        files: {
+          'dist/css/pretty.css': 'src/css/pretty.scss'
+        }
+      }
+    },
+
+    // configure cssmin to minify css files ------------------------------------
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/css/style.min.css': 'src/css/style.css'
+        }
+      }
+    },
+
+    // configure watch to auto update ----------------
+    watch: {
+      
+      // for stylesheets, watch css and less files 
+      // only run less and cssmin stylesheets:
+      files: ['src/**/*.css', 'src/**/*.scss'], 
+      tasks: ['sass', 'cssmin'],
+
+      // for scripts, run jshint and uglify 
+      scripts: { 
+        files: 'src/**/*.js', tasks: ['jshint', 'uglify'] 
+      } 
+    }
+
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  //grunt.registerTask('default', ['sass']);
+
+};
